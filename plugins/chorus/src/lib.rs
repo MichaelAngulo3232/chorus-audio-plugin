e5b828783106ce707e7cf843ac9ecf231a62dce2
Michael Angulo <MichaelAngulo3232@gmail.com>
2025-05-17 21:34:57 -0700

    Start of Plugin implementation

--- plugins/chorus/src/lib.rs
+++ plugins/chorus/src/lib.rs
@@ -7,3 +7,31 @@ struct Chorus {
     lfo_phase: f32,
     sample_rate: f32
 }
+
+impl Default for Chorus {
+
+    fn default() -> Self {
+        let max_delay_samples = (0.02 * 48000.0) as usize;
+        Self {
+            delay_buffer: vec![0.0; max_delay_samples],
+            write_pos: 0,
+            lfo_phase: 0.0,
+            sample_rate: 48000.0,
+        }
+    }
+}
+
+impl Plugin for Chorus {
+    
+    const NAME: &'static str = "Crimson";
+    const VENDOR: &'static str = "Pyfessional";
+    const URL: &'static str = "https://pyfessional.tech";
+    const EMAIL: &'static str = "N/A";
+
+
+
+
+
+
+   // End of Plugin block 
+}
