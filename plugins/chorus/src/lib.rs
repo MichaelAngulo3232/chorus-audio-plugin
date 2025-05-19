3a073d5558d74acf5127c63fee5f58d18dcebc0e
Michael Angulo <MichaelAngulo3232@gmail.com>
2025-05-19 00:03:13 -0700

    Implementing all the required functions. After this I just need to add the Process function.

--- plugins/chorus/src/lib.rs
+++ plugins/chorus/src/lib.rs
@@ -27,7 +27,32 @@ impl Plugin for Chorus {
     const VENDOR: &'static str = "Pyfessional";
     const URL: &'static str = "https://pyfessional.tech";
     const EMAIL: &'static str = "N/A";
+    const VERSION: &'static str = "0.1.0";
 
+    type SysExMessage = ();
+
+    fn params(&self) -> Arc<dyn Params> {
+        Arc::new(EmptyParams)
+    }
+
+    fn initialize(
+        &mut self, 
+        buffer_config: &BufferConfig,
+        _config: &mut impl InitContext<self>,
+    ) -> bool {
+        self.sample_rate = buffer_config.sample_rate
+        let max_delay_samples = (0.02 * self.sample_rate) as usize;
+        self.delay_buffer = vec![0.0; max_delay_samples];
+        true
+    }
+
+    fn reset(&mut self) {
+        self.write_pos = 0;
+        self.lfo_phase = 0.0;
+        self.delay_buffer.fill(0.0);
+    }
+
+    
 
 
 
